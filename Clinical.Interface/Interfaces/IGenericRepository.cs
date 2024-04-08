6a7a54557f717a3fb538a8bc3b61125d5311e4e6
DESKTOP-SC4J70J\Jasson__cu <jassoncucaal14@hotmail.com>
2024-04-08 17:29:16 -0600

    Add: `UnitOfWork` and `GenericRepository` Design patterns.

---
+++ Clinical.Interface/Interfaces/IGenericRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Clinical.Interface.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync(string storeProcedure);
+        Task<T> GetByIdAsync(string storeProcedure, object parameter);
+        Task<bool> ExecAsync(string storeProcedure, object parameter);
+    }
+}
