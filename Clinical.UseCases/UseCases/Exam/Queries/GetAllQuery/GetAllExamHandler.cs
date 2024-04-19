12b77aeabca49b37461ea2a4145be3cbc5cb7323
DESKTOP-SC4J70J\Jasson__cu <jassoncucaal14@hotmail.com>
2024-04-18 18:44:28 -0600

    feat: Add `GetAllExamQuery` and `GetAllExamHandler`
    Introduce GetAllExamQuery class for retrieving all exam data
    and GetAllExamHandler to handle GetAllExamQuery.

---
+++ Clinical.UseCases/UseCases/Exam/Queries/GetAllQuery/GetAllExamHandler.cs
@@ -0,0 +1,41 @@
+﻿using Clinical.Application.DTOS.Exam.Response;
+using Clinical.Interface.Interfaces;
+using Clinical.UseCases.Commons.Bases;
+using Clinical.Utils.Constants;
+using MediatR;
+
+namespace Clinical.UseCases.UseCases.Exam.Queries.GetAllQuery
+{
+    public class GetAllExamHandler : IRequestHandler<GetAllExamQuery, BaseResponse<IEnumerable<GetAllExamResponseDto>>>
+    {
+        private readonly IExamRepository _examRepository;
+
+        public GetAllExamHandler(IExamRepository examRepository)
+        {
+            _examRepository = examRepository;
+        }
+
+        public async Task<BaseResponse<IEnumerable<GetAllExamResponseDto>>> Handle(GetAllExamQuery request, CancellationToken cancellationToken)
+        {
+            var response = new BaseResponse<IEnumerable<GetAllExamResponseDto>>();
+
+            try
+            {
+                var exams = await _examRepository.GetAllExam(StoreProcedures.uspExamList);
+
+                if (exams is not null)
+                {
+                    response.IsSuccess = true;
+                    response.Data = exams;
+                    response.Message = "Consulta exitosa";
+                }
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+            }
+
+            return response;
+        }
+    }
+}
