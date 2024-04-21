6d8f5c69901e6ae79bdb3085924252f1ce4717d5
DESKTOP-SC4J70J\Jasson__cu <jassoncucaal14@hotmail.com>
2024-04-20 20:32:48 -0600

    Add: Handler for `Exam` response

---
+++ Clinical.UseCases/UseCases/Exam/Queries/GetByIdQuery/GetExamByIdHandler.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Clinical.Application.DTOS.Exam.Response;
+using Clinical.Interface.Interfaces;
+using Clinical.UseCases.Commons.Bases;
+using Clinical.Utils.Constants;
+using MediatR;
+
+namespace Clinical.UseCases.UseCases.Exam.Queries.GetByIdQuery
+{
+    public class GetExamByIdHandler : IRequestHandler<GetExamByIdQuery, BaseResponse<GetExamByIdResponseDto>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetExamByIdHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<BaseResponse<GetExamByIdResponseDto>> Handle(GetExamByIdQuery request, CancellationToken cancellationToken)
+        {
+            var response = new BaseResponse<GetExamByIdResponseDto>();
+            try
+            {
+                var exam = await _unitOfWork.Exam.GetByIdAsync(StoreProcedures.uspExamById, request);
+
+                if (exam is null)
+                {
+                    response.IsSuccess = false;
+                    response.Message = GlobalMessage.MESSAGE_QUERY_EMPTY;
+                    return response;
+                }
+
+                response.IsSuccess = true;
+                response.Data = _mapper.Map<GetExamByIdResponseDto>(exam);
+                response.Message = GlobalMessage.MESSAGE_QUERY;
+
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+            }
+
+            return response;
+        }
+    }
+}
