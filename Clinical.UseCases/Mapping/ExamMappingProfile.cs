2f4e1a5f185184ec6f9c54f12a916f0471e91a60
DESKTOP-SC4J70J\Jasson__cu <jassoncucaal14@hotmail.com>
2024-04-20 20:27:46 -0600

    feat: add `MappingProfile` for `Exam` Entity

---
+++ Clinical.UseCases/Mapping/ExamMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Clinical.Application.DTOS.Exam.Response;
+using Clinical.Domain.Entities;
+
+namespace Clinical.UseCases.Mapping
+{
+    public class ExamMappingProfile : Profile
+    {
+        public ExamMappingProfile()
+        {
+            CreateMap<Exam, GetExamByIdResponseDto>()
+                .ReverseMap();
+        }
+    }
+}
