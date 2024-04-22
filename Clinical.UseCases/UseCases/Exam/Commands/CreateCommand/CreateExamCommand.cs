906fede895ebeed9dad8c172230823e9da424434
DESKTOP-SC4J70J\Jasson__cu <jassoncucaal14@hotmail.com>
2024-04-22 08:57:27 -0600

    feat: create `CreateExamCommand` for Exams Register

---
+++ Clinical.UseCases/UseCases/Exam/Commands/CreateCommand/CreateExamCommand.cs
@@ -0,0 +1,11 @@
+﻿using Clinical.UseCases.Commons.Bases;
+using MediatR;
+
+namespace Clinical.UseCases.UseCases.Exam.Commands.CreateCommand
+{
+    public class CreateExamCommand : IRequest<BaseResponse<bool>>
+    {
+        public string? Name { get; set; }
+        public int AnalysisId { get; set; }
+    }
+}
