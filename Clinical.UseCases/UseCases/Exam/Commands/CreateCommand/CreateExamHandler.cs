4f9fa5ffe6192e12251f7591fab0a4bf863db66b
DESKTOP-SC4J70J\Jasson__cu <jassoncucaal14@hotmail.com>
2024-04-22 08:58:19 -0600

    feat: add `Handler` for Exam Register

---
+++ Clinical.UseCases/UseCases/Exam/Commands/CreateCommand/CreateExamHandler.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Clinical.Interface.Interfaces;
+using Clinical.UseCases.Commons.Bases;
+using Clinical.Utils.Constants;
+using Clinical.Utils.HelperExtensions;
+using MediatR;
+using Entity = Clinical.Domain.Entities;
+
+namespace Clinical.UseCases.UseCases.Exam.Commands.CreateCommand
+{
+    public class CreateExamHandler : IRequestHandler<CreateExamCommand, BaseResponse<bool>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public CreateExamHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<BaseResponse<bool>> Handle(CreateExamCommand request, CancellationToken cancellationToken)
+        {
+            var response = new BaseResponse<bool>();
+
+            try
+            {
+                var exam = _mapper.Map<Entity.Exam>(request);
+                var parameters = exam.GetPropertiesWithValues();
+                response.Data = await _unitOfWork.Exam.ExecAsync(StoreProcedures.uspExamRegister, parameters);
+
+                if (response.Data)
+                {
+                    response.IsSuccess = true;
+                    response.Message = GlobalMessage.MESSAGE_SAVE;
+                }
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+            }
+
+            return response;
+        }
+    }
+}
