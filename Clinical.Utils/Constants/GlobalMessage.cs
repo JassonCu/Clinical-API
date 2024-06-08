d81ed067c701aee2d05de2ff8f17a52e7d867fe3
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-06-08 12:39:15 -0600

    fix: :bug: Change the email address for `MESSAGE_EXCEPTION` const

--- Clinical.Utils/Constants/GlobalMessage.cs
+++ Clinical.Utils/Constants/GlobalMessage.cs
@@ -14,6 +14,6 @@
         public const string MESSAGE_TOKEN_ERROR = "El usuario y/o contraseña es incorrecta, compruébala.";
         public const string MESSAGE_VALIDATE = "Errores de validación.";
         public const string MESSAGE_FAILED = "Operación fallida.";
-        public const string MESSAGE_EXCEPTION = "Hubo un error inesperado, comunicarse con el administrador (jasson14@hotmail.com).";
+        public const string MESSAGE_EXCEPTION = "Hubo un error inesperado, comunicarse con el administrador (jassoncucaal14@hotmail.com).";
     }
 }
